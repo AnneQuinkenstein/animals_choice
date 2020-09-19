393570c0e6e82bebd606ca1c053bdcb3acfd3739
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-19 15:32:36 +0200

    fixed backgroud-pic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ const Navbar = () => {
         <nav>
             <div className="logo-wrapper">
                 <a href="https://www.linkedin.com/in/anne-quinkenstein/"><img src="/images/logo.png" alt='logo AQ' /> </a>
-                <p> AQUIN <br/> Animals Choice</p>
+                <p>  <br/> Animals Choice</p>
             </div>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="cart">Cart</Link></li>
             </ul>
-            <div>
+            <div className="nav-cart">
                 <p><mark>Total Price </mark> <span>&nbsp;&nbsp;0&nbsp;&nbsp;</span> Yards of Linen</p>
             </div>
         </nav>
