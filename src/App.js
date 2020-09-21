015482c86e6ed927d47d415b313eb29323ff22f1
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-21 22:07:49 +0200

    pushed footer to bottom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React, { useEffect, useState}from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home'; 
-import Navbar from './components/Navbar'; 
-import Footer from './components/Footer'; 
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Cart from './components/Cart';
-import ProductDetails from './components/ProductDetails'; 
-import productsdata from './data'; 
+import ProductDetails from './components/ProductDetails';
+import productsdata from './data';
 
 function App() {
-  const [ products, setProducts ] = useState(null);
+  const [products, setProducts] = useState(null);
   // const [ totalPrice, setTotalPrice ] = useState(0)
-  const totalPrice = 0; 
+  const totalPrice = 0;
 
   // fetch('https://floating-temple-56492.herokuapp.com/products')
   //     .then(res => res.json())
   //     .then(data => setProducts(data))
 
   useEffect(() => {
-   setProducts(productsdata); 
+    setProducts(productsdata);
   }, [])
 
-console.log(products);
+  console.log(products);
   return (
     <div className="wrapper">
-      <Navbar totalPrice={totalPrice}/> 
-      <Routes>
-        <Route path="/" element={<Home  products={products}/>} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="product/:id"  element={ products && <ProductDetails products={products}/> }/>   
-      </Routes>
-      <Footer/>
+        <Navbar totalPrice={totalPrice} />
+        <div className="maincontent">
+         <Routes>
+          <Route path="/" element={<Home products={products} />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="product/:id" element={products && <ProductDetails products={products} />} />
+        </Routes> 
+        </div>
+        <Footer />
     </div>
   );
 }
