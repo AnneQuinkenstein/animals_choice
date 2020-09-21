9b5922e88d99f2925c77a76b8a30c6d4042407f1
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-21 23:25:04 +0200

    Footer Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.linkedin.com/in/anne-quinkenstein/">buy me</a>
-            <p>  &nbsp;&nbsp;or&nbsp;&nbsp; </p>
-            <a href="https://github.com/AnneQuinkenstein">look at my code</a>
+            <div>
+                {/* <a href="https://www.linkedin.com/in/anne-quinkenstein/">buy me</a> */}
+                {/* <p>  &nbsp;&nbsp;or&nbsp;&nbsp; </p> */}
+                <a href="https://github.com/AnneQuinkenstein">look at my code</a>
+            </div>
         </div>
     )
 }
