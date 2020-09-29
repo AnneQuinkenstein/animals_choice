c2999a38bac1999ce3bb49e2d6101744bb0b37c6
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-29 23:13:33 +0200

    material icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AnimalsContext } from "../contexts/AnimalsContext";
+import { BottomNavigationAction, Tooltip } from "@material-ui/core";
+import HomeTwoToneIcon from "@material-ui/icons/HomeTwoTone";
+import ShoppingCartTwoToneIcon from '@material-ui/icons/ShoppingCartTwoTone';
 
 const Navbar = () => {
   const { totalPrice } = useContext(AnimalsContext);
@@ -14,17 +17,25 @@ const Navbar = () => {
         <p>Animals Choice</p>
       </div>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="cart">Cart</Link>
-        </li>
+        <Tooltip title="Home">
+          <li>
+            <Link to="/">
+              <BottomNavigationAction icon={<HomeTwoToneIcon style={{ fontSize: 60 }}/>} label="Home" />
+            </Link>
+          </li>
+        </Tooltip>
+        <Tooltip title="Cart">
+          <li>
+            <Link to="cart">
+              <BottomNavigationAction icon={<ShoppingCartTwoToneIcon style={{ fontSize: 60 }}/>} label="Cart" />
+            </Link>
+          </li>
+        </Tooltip>
       </ul>
       <div className="nav-cart">
         <p>
           <mark>Total Price </mark>{" "}
-          <span>&nbsp;&nbsp;{totalPrice}&nbsp;&nbsp; Yards of Linen </span> 
+          <span>&nbsp;&nbsp;{totalPrice}&nbsp;&nbsp; Yards of Linen </span>
         </p>
       </div>
     </nav>
