9e66cc2c00dda716ce11ac53b546d92ec19be073
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-29 22:09:15 +0200

    changed ccs on TotalPrice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AnimalsContext } from '../contexts/AnimalsContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AnimalsContext } from "../contexts/AnimalsContext";
 
 const Navbar = () => {
-    const { totalPrice } = useContext(AnimalsContext);
+  const { totalPrice } = useContext(AnimalsContext);
 
-    return (
-        <nav>
-            <div>
-                <div className="logo-wrapper">
-                    <a href="https://www.linkedin.com/in/anne-quinkenstein/"><img src="/images/logo.png" alt='logo AQ' /> </a>
-                    <p>Animals Choice</p>
-                </div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="cart">Cart</Link></li>
-                </ul>
-            </div>
-            <div className="nav-cart">
-                <p><mark>Total Price </mark> <span>&nbsp;&nbsp;{totalPrice}&nbsp;&nbsp;</span> Yards of Linen</p>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <div className="logo-wrapper">
+        <a href="https://www.linkedin.com/in/anne-quinkenstein/">
+          <img src="/images/logo.png" alt="logo AQ" />{" "}
+        </a>
+        <p>Animals Choice</p>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="cart">Cart</Link>
+        </li>
+      </ul>
+      <div className="nav-cart">
+        <p>
+          <mark>Total Price </mark>{" "}
+          <span>&nbsp;&nbsp;{totalPrice}&nbsp;&nbsp; Yards of Linen </span> 
+        </p>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar; 
+export default Navbar;
