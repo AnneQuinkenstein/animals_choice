913f7c607659bede3e6012e84844fd9ad3273efd
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-30 11:41:28 +0200

    extra card-details cardcomponent to style ProductInfos

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,40 +1,32 @@
-import React, { useContext } from 'react';
-import StarRatings from 'react-star-ratings';
-import { useParams } from 'react-router';
-import { AnimalsContext } from '../contexts/AnimalsContext';
-
+import React, { useContext } from "react";
+import { useParams } from "react-router";
+import { AnimalsContext } from "../contexts/AnimalsContext";
+import CardComponent from "./CartComponent"; 
 
 const ProductDetails = () => {
-    const { id } = useParams();
-    const { products } = useContext(AnimalsContext);
+  const { id } = useParams();
+  const { products } = useContext(AnimalsContext);
+  
 
-    const matchProduct = products && products.find((product) => (product.id === +id))
+  const matchProduct =
+    products && products.find((product) => product.id === +id);
 
-    
-    return (
-        <div className="productdetails">
-            <div className="info-wrapper">
-                <h1>{matchProduct.product_title}</h1>
-                <p>{matchProduct.product_description}</p>
-                <div>
-                   <StarRatings
-                    rating={matchProduct.product_rating}
-                    starRatedColor="orange"
-                    starDimension="20px"
-                />  
-                </div>
-                <p><mark>Price </mark><span>&nbsp;&nbsp;{matchProduct.product_price}&nbsp;&nbsp;</span>   Yards of Linen</p>
-            </div>
-            <div className="item-wrapper">
-                <span></span>
-                <span></span>
-                <span></span>
-                <div className="image-wrapper">
-                    <img src={matchProduct.product_image} alt={matchProduct.product_title} />
-                </div>
-            </div>
+  return (
+    <div className="productdetails">
+      <CardComponent matchProduct={matchProduct} />
+      <div className="item-wrapper">
+        <span></span>
+        <span></span>
+        <span></span>
+        <div className="image-wrapper">
+          <img
+            src={matchProduct.product_image}
+            alt={matchProduct.product_title}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ProductDetails; 
+export default ProductDetails;
