c3b86c619088f290db6b0826661c6ef42e6a5cf3
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-30 14:04:10 +0200

    added rel='noopener noreferrer' to target='_blank' for securtiy reasons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div>
-                <a href="https://github.com/AnneQuinkenstein" target="_blank">look at my code</a>
+                <a href="https://github.com/AnneQuinkenstein" target="_blank" rel="noopener noreferrer">look at my code</a>
             </div>
         </div>
     )
